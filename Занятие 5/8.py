4904a23c30735269b3f2c5072f6797d7aee183a2
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:11:34 +0300

    Create 8.py

---
+++ Занятие 5/8.py
@@ -0,0 +1,10 @@
+c = 0
+previous = None
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    if previous is not None and n == previous: 
+        c += 1
+    previous = n
+print(c)
