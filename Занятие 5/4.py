129ad332bd0e6869be9c50beca0ee0217b8881d0
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:52:13 +0300

    Update 4.py

--- Занятие 5/4.py
+++ Занятие 5/4.py
@@ -1,7 +1,11 @@
+def F(x, y)
+    d = 1
+    while x < y:
+        x = x + x * 0.10
+        d += 1
+    print(d)
+
 x = float(input())
 y = float(input())
-d = 1
-while x < y:
-    x = x + x * 0.10
-    d += 1
-print(d)
+
+F(x, y)
