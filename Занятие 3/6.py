7755977dc99ad6d8aba7d8a1138772e9c500ea11
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:25:00 +0300

    Update 6.py

--- Занятие 3/6.py
+++ Занятие 3/6.py
@@ -1,18 +1,19 @@
-n1 = int(input())
-n2 = int(input())
-m1 = int(input())
-m2 = int(input())
-d1 = 0
-d2 = 0
-if n1 or n2 %2==0:
-    d1 = 1
-else:
+def F(n1, n2, m1, m2)
+    n1 = int(input())
+    n2 = int(input())
+    m1 = int(input())
+    m2 = int(input())
     d1 = 0
-if m1 or m2 %2==0:
-    d2 = 1
-else:
     d2 = 0
-if d1 == d2:
-    print('Да')
-else:
-    print('Нет')
+    if n1 or n2 %2==0:
+        d1 = 1
+    else:
+        d1 = 0
+    if m1 or m2 %2==0:
+        d2 = 1
+    else:
+        d2 = 0
+    if d1 == d2:
+        print('Да')
+    else:
+        print('Нет')
