93794219e5bfa0aee3a5621d5c3b44734eb9ef33
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:51:13 +0300

    Update 2.py

--- Занятие 5/2.py
+++ Занятие 5/2.py
@@ -1,6 +1,10 @@
+def F(n):
+    if n > 2:
+        for i in range(1, n + 1):
+            if n % i == 0 and i!=1:
+                print(i)
+                break
+
 n = int(input())
-if n > 2:
-    for i in range(1, n + 1):
-        if n % i == 0 and i!=1:
-            print(i)
-            break
+
+F(n)
