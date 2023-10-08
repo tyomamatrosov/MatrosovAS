43058d761f480159f533c88818039cad86e4495c
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:52:49 +0300

    Update 5.py

--- Занятие 5/5.py
+++ Занятие 5/5.py
@@ -1,7 +1,11 @@
-c = 0
-while True:
-    n  = int(input())
-    if n == 0:
-        break
-    c += 1
-print(c)
+def F(n):
+    c = 0
+    while True:
+        if n == 0:
+            break
+        c += 1
+    print(c)
+
+n  = int(input())
+
+F(n)
