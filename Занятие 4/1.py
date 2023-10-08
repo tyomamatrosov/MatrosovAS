cb4b1a270c28671e71331381191d25cc8ef1a35d
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:45:08 +0300

    Update 1.py

--- Занятие 4/1.py
+++ Занятие 4/1.py
@@ -1,8 +1,11 @@
-A = int(input()) 
-B = int(input()) 
-
-if A <= B:
+def F(A, B):
+  if A <= B:
   for i in range(A, B + 1):
     print(i)
   else:
     print("A должно быть меньше или равно B")
+
+A = int(input()) 
+B = int(input()) 
+
+F(A, B)
