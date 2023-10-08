8018208a3cff951699b1f0cbe1097e5f5721af36
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-08 18:09:04 +0300

    Update 8.py

--- Занятие 4/8.py
+++ Занятие 4/8.py
@@ -1,15 +1,8 @@
-def F(n):
-    r = 1
-    for i in range(2, n + 1):
-        r *= i
-    return r
-
-def SF(n):
-    sr = 0
-    for i in range(1, n + 1):
-        sr += F(i)
-    return sr
-
 n = int(input())
-r = SF(n)
-print(r) 
+if 1 <= n <= 9:
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print(j, end="")
+        print()  
+else:
+    print("Ошибка")
