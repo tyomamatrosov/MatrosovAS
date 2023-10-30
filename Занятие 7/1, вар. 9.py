ceecee2a1fc4ae960b451cb11e433c24abd6294c
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-30 08:31:07 +0300

    Update 1, вар. 9.py

--- Занятие 7/1, вар. 9.py
+++ Занятие 7/1, вар. 9.py
@@ -1,8 +1,9 @@
-a = []
-N = int(input())
-for i in range (N):
-    a.append(float(input()))
-minimalabs = min(a, key=abs) #Минимальный по модулю элемент
-print(minimalabs)
-a.sort(reverse = 1) #Массив в обратном порядке
-print(a)
+def F(x)
+    a = []
+    for i in range (x):
+        a.append(float(input()))
+    minimalabs = min(a, key=abs) #Минимальный по модулю элемент
+    print(minimalabs)
+    a.sort(reverse = 1) #Массив в обратном порядке
+    print(a)
+F(int(input()))
