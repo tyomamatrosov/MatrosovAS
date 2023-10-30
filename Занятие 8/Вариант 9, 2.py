690e32f39e46e968337db0de5e5b4777576f388c
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-10-30 08:58:19 +0300

    Update Вариант 9, 2.py

--- Занятие 8/Вариант 9, 2.py
+++ Занятие 8/Вариант 9, 2.py
@@ -1,25 +1,10 @@
-def calculate_product_and_mean(arr):
-    # Находим произведение элементов
-    product = 1
-    for num in arr:
-        product *= num
-    
-    # Находим среднеарифметическое значение
-    mean = sum(arr) / len(arr)
-    
-    return product, mean
+def F(x):
+    p = 1
+    for n in x:
+        p *= n
+    s = sum(x) / len(x)
+    print(p, s) #Возврат произведения и среднего арифметического
 
-# Примеры массивов
-array1 = [2, 3, 4, 5]
-array2 = [1, 2, 3, 4, 5]
-array3 = [10, 20, 30]
-
-# Вычисляем произведение и среднеарифметическое значение для каждого массива
-product1, mean1 = calculate_product_and_mean(array1)
-product2, mean2 = calculate_product_and_mean(array2)
-product3, mean3 = calculate_product_and_mean(array3)
-
-# Выводим результаты
-print("Массив 1: Произведение =", product1, "Среднее =", mean1)
-print("Массив 2: Произведение =", product2, "Среднее =", mean2)
-print("Массив 3: Произведение =", product3, "Среднее =", mean3)
+F([2, 3, 4, 5])
+F([1, 2, 3, 4, 5])
+F([10, 20, 30])
