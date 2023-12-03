7425154a801a2df175d25c840dcd54abf546ebd7
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2023-12-03 20:45:21 +0300

    Update Вариант 9, 2.py

--- Занятие 10/Вариант 9, 2.py
+++ Занятие 10/Вариант 9, 2.py
@@ -11,12 +11,10 @@ def F(matrix):
 
     return max_indices
 
-# Функция для удаления строки и столбца с заданными индексами из матрицы
 def R(matrix, row, col):
     new_matrix = [row[:col] + row[col + 1:] for i, row in enumerate(matrix) if i != row]
     return [list(x) for x in zip(*new_matrix)]
 
-# Функция для чтения матрицы из файла
 def Read(file_path):
     matrix = []
 
@@ -27,25 +25,21 @@ def Read(file_path):
 
     return matrix
 
-# Функция для записи матрицы в файл
 def W(file_path, matrix):
     with open(file_path, 'w') as file:
         for row in matrix:
             line = ' '.join(map(str, row)) + '\n'
             file.write(line)
 
-# Чтение матрицы из файла
 file_path_input = "Матросов_У-234_vvod.txt"
 matrix = Read(file_path_input)
 
-# Поиск индексов максимального по модулю элемента
 max_indices = F(matrix)
 
 if max_indices:
-    # Удаление строки и столбца с максимальным элементом
+  
     new_matrix = R(matrix, max_indices[0], max_indices[1])
 
-    # Вывод результата 
     print("Исходная матрица:")
     for row in matrix:
         print(row)
@@ -56,7 +50,6 @@ if max_indices:
     for row in new_matrix:
         print(row)
 
-    # Запись результата в файл
     file_path_output = "Матросов_У-234_vivod2.txt"
     W(file_path_output, new_matrix)
 else:
