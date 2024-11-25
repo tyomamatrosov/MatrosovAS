c277ee517590864bd05a4e0b91439cdfacc8e8f2
tyomamatrosov <143992287+tyomamatrosov@users.noreply.github.com>
2024-11-26 02:53:58 +0300

    Update DepartmentService.java

--- hospital/src/main/java/services/DepartmentService.java
+++ hospital/src/main/java/services/DepartmentService.java
@@ -9,18 +9,15 @@ public class DepartmentService {
     private List<Department> departments = new ArrayList<>();
     private int nextDepartmentId = 1;
 
-    // Метод для добавления отделения
     public void addDepartment(Department department) {
         department.setId(nextDepartmentId++);
         departments.add(department);
     }
 
-    // Метод для удаления отделения по ID
     public boolean removeDepartment(int id) {
         return departments.removeIf(department -> department.getId() == id);
     }
 
-    // Метод для получения всех отделений
     public List<Department> getAllDepartments() {
         return departments;
     }
